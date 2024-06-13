0eb56aef9ce11ecd3cd733839585e45a287786b0
lcho3878 <leech3878@naver.com>
2024-06-14 02:08:32 +0900

    Chore: OrangeButton 생성

--- MeaningOut/CustomView/OrangeButton.swift
+++ MeaningOut/CustomView/OrangeButton.swift
@@ -5,4 +5,20 @@
 //  Created by 이찬호 on 6/14/24.
 //
 
-import Foundation
+import UIKit
+
+class OrangeButton: UIButton {
+    
+    init(buttonType: Constant.ButtonType) {
+        super.init(frame: CGRect.zero)
+        
+        setTitle(buttonType.rawValue, for: .normal)
+        backgroundColor = Constant.AppColor.orange
+        layer.cornerRadius = 25
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
