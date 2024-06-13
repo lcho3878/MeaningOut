acf426f0a8eca92b2d9ef3fa6cc321277e8ec803
lcho3878 <leech3878@naver.com>
2024-06-14 02:54:50 +0900

    Chore: UIView Extension makeCircle 함수 생성

---
+++ MeaningOut/Extension/UIView+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  UIView+Extension.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import UIKit
+
+extension UIView {
+    
+    func makeCircle() {
+        layer.cornerRadius = frame.width / 2
+    }
+    
+}
