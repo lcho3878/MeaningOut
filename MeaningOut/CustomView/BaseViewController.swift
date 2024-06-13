ad067f259ca3745b425fddaa9825654e9b906344
lcho3878 <leech3878@naver.com>
2024-06-14 01:40:50 +0900

    Feat: CodeBasable 프로토콜, BaseViewController 생성

---
+++ MeaningOut/CustomView/BaseViewController.swift
@@ -0,0 +1,17 @@
+//
+//  BaseViewController.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+    }
+    
+}
---
+++ MeaningOut/Protocol/CodeBasable.swift
@@ -0,0 +1,16 @@
+//
+//  CodeBasable.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import Foundation
+
+protocol CodeBasable {
+    
+    func configureHierarchy ()
+    
+    func configureLayout ()
+    
+}
