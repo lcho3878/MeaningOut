c82fce5e3856a881c0aa81d77ab4857cf2f1d323
lcho3878 <leech3878@naver.com>
2024-06-14 02:10:21 +0900

    Comment: BaseViewConrtoller 주석

--- MeaningOut/CustomView/BaseViewController.swift
+++ MeaningOut/CustomView/BaseViewController.swift
@@ -9,6 +9,10 @@ import UIKit
 
 class BaseViewController: UIViewController, CodeBasable {
 
+    /*
+     configure함수들을 viewDidLoad에서 실행하므로 상속받은 클래스에서는
+     configure함수를 구현하여 사용하면 자동으로 실행함
+     */
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
