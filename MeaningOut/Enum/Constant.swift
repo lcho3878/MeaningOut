c07368363410f692d60343445f306b82b68290be
lcho3878 <leech3878@naver.com>
2024-06-14 01:55:27 +0900

    Chore: Constant, AppColor Enum 생성

---
+++ MeaningOut/Enum/Constant.swift
@@ -0,0 +1,19 @@
+//
+//  Constant.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import UIKit
+
+enum Constant {
+    enum AppColor {
+        static let orange = UIColor(hexCode: "EF8947")
+        static let black = UIColor(hexCode: "000000")
+        static let white = UIColor(hexCode: "FFFFFF")
+        static let lightGray = UIColor(hexCode: "CDCDCD")
+        static let middleGray = UIColor(hexCode: "828282")
+        static let heavyGray = UIColor(hexCode: "4C4C4C")
+    }
+}
