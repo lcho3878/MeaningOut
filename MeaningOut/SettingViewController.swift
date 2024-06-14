127b3a98150e17f9f4e9fddaaa10e0ca07b26a55
lcho3878 <leech3878@naver.com>
2024-06-14 19:44:20 +0900

    Feat: SettingViewController UI 및 오토레이아웃 작성

--- MeaningOut/SettingViewController.swift
+++ MeaningOut/SettingViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import SnapKit
 
 class SettingViewController: BaseViewController {
 
     private let viewTyle = Constant.ViewType.setting
     
+    private let settingMenus = Constant.SettingMenu.allCases
+
+    
+    private let settingTableView = {
+        let settingTableView = UITableView()
+        return settingTableView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        configureTableView()
     }
     
     override func configureNavigationItem() {
@@ -21,12 +30,50 @@ class SettingViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        
+        view.addSubview(settingTableView)
     }
     
     override func configureLayout() {
         
+        settingTableView.snp.makeConstraints {
+            $0.edges.equalTo(view.safeAreaLayoutGuide)
+        }
     }
 
 
+}
+
+extension SettingViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    private func configureTableView() {
+        settingTableView.delegate = self
+        settingTableView.dataSource = self
+        settingTableView.isScrollEnabled = false
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return section == 0 ? 1 : settingMenus.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell()
+//        if indexPath.section == 0 {
+//
+//        }
+//        else {
+//
+//        }
+
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return indexPath.section == 0 ? 160 : UITableView.automaticDimension
+    }
+    
+    
 }
