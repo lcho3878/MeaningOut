a2b00d23541b08be0c4a7f1c60a40afd4f008214
lcho3878 <leech3878@naver.com>
2024-06-14 19:51:43 +0900

    Chore: UITableViewCell Extension 생성 및 Reusable 프로토콜 채택

---
+++ MeaningOut/Extension/UITableViewCell+Extension.swift
@@ -0,0 +1,14 @@
+//
+//  UITableViewCell+Extension.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import UIKit
+
+extension UITableViewCell: Reusable {
+    static var id: String {
+        return String(describing: self)
+    }
+}
---
+++ MeaningOut/SettingView/ProfileCell.swift
@@ -0,0 +1,23 @@
+//
+//  ProfileCell.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import UIKit
+
+class ProfileCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
