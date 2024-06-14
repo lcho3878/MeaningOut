2e8562cbc8cddc159e3b400ed4c64bc57eb85383
lcho3878 <leech3878@naver.com>
2024-06-14 17:56:49 +0900

    Chore: TabBarController 생성

---
+++ MeaningOut/CustomView/TabBarController.swift
@@ -0,0 +1,17 @@
+//
+//  TabBarController.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/14/24.
+//
+
+import UIKit
+
+class TabBarController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
