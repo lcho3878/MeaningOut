e880041979ed4a627bddaef083dd113364df154a
lcho3878 <leech3878@naver.com>
2024-06-15 15:58:31 +0900

    Chore: SearchResultCollectionViewCell 생성

---
+++ MeaningOut/SearchResultCollectionViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  SearchResultCollectionViewCell.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/15/24.
+//
+
+import UIKit
+
+class SearchResultCollectionViewCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+}
