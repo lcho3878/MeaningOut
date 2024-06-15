32dc75bfc4450d069646604e24c81557df6998b8
lcho3878 <leech3878@naver.com>
2024-06-15 13:36:14 +0900

    Feat: MainViewController empty UI 오토레이아웃 작성

--- MeaningOut/MainViewController.swift
+++ MeaningOut/MainViewController.swift
@@ -6,10 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class MainViewController: BaseViewController {
     
     private let viewType = Constant.ViewType.main
+    
+    private let searchBar = {
+        let searchBar = UISearchBar()
+        searchBar.searchTextField.placeholder = "브랜드, 상품등을 입력하세요"
+        return searchBar
+    }()
+    
+    private let emptyImageView = {
+        let emptyImageView = UIImageView()
+        emptyImageView.image = UIImage.empty
+        emptyImageView.contentMode = .scaleAspectFit
+        return emptyImageView
+    }()
+    
+    private let emptyLabel = {
+        let emptyLabel = UILabel()
+        emptyLabel.text = "최근 검색어가 없어요"
+        emptyLabel.textColor = Constant.AppColor.black
+        emptyLabel.font = Constant.FontSize.titleBold
+        emptyLabel.textAlignment = .center
+        return emptyLabel
+    }()
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -20,11 +43,26 @@ class MainViewController: BaseViewController {
     }
     
     override func configureHierarchy() {
-        
+        view.addSubview(searchBar)
+        view.addSubview(emptyImageView)
+        view.addSubview(emptyLabel)
     }
     
     override func configureLayout() {
+        searchBar.snp.makeConstraints {
+            $0.top.horizontalEdges.equalTo(view.safeAreaLayoutGuide)
+        }
         
+        emptyImageView.snp.makeConstraints {
+            $0.center.equalToSuperview()
+            $0.height.equalToSuperview().multipliedBy(0.5)
+            $0.horizontalEdges.equalToSuperview()
+        }
+        
+        emptyLabel.snp.makeConstraints {
+            $0.centerX.equalToSuperview()
+            $0.top.equalTo(emptyImageView.snp.bottom)
+        }
     }
     
     // notification center? 사용 방식도 고려해볼것
