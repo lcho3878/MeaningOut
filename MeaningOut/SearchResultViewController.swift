2a3d927f9f9a5c6779a4cf763e6a7041260889c6
lcho3878 <leech3878@naver.com>
2024-06-15 14:53:51 +0900

    Chore: SearchResultViewController 생성

---
+++ MeaningOut/SearchResultViewController.swift
@@ -0,0 +1,30 @@
+//
+//  SearchResultViewController.swift
+//  MeaningOut
+//
+//  Created by 이찬호 on 6/15/24.
+//
+
+import UIKit
+
+class SearchResultViewController: BaseViewController {
+    
+    private let viewType = Constant.ViewType.result
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func configureNavigationItem() {
+        
+    }
+    
+    override func configureHierarchy() {
+        
+    }
+    
+    override func configureLayout() {
+        
+    }
+}
