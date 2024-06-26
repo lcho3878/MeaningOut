0d63d6e2977a4fa42101d784ee785a9353d1d50c
lcho3878 <leech3878@naver.com>
2024-06-26 15:47:59 +0900

    Refactor: UserDataKey enum 생성 및 코드 리팩토링

--- MeaningOut/Struct/User.swift
+++ MeaningOut/Struct/User.swift
@@ -8,59 +8,60 @@
 import UIKit
 
 struct User {
+    typealias key = UserDataKey
     
     static var nickanme: String? {
         get {
-            guard let nickname = UserDefaults.standard.string(forKey: "nickname") else {
+            guard let nickname = UserDefaults.standard.string(forKey: key.nickname.rawValue) else {
                 return nil
             }
             return nickname
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "nickname")
+            UserDefaults.standard.set(newValue, forKey: key.nickname.rawValue)
         }
     }
     
     static var profileImage: UIImage? {
         get {
-            guard let imageAssetName = UserDefaults.standard.string(forKey: "profileImageAssetName") else { return nil }
+            guard let imageAssetName = UserDefaults.standard.string(forKey: key.profileImageAssetName.rawValue) else { return nil }
             return UIImage(named: imageAssetName)
         }
         set {
             guard let imageAssetName = newValue?.imageAsset?.value(forKey: "assetName") else { return }
-            UserDefaults.standard.set(imageAssetName, forKey: "profileImageAssetName")
+            UserDefaults.standard.set(imageAssetName, forKey: key.profileImageAssetName.rawValue)
         }
     }
     
     static var signupDate: String? {
         get {
-            guard let signupDate = UserDefaults.standard.string(forKey: "signupDate") else {
+            guard let signupDate = UserDefaults.standard.string(forKey: key.signupDate.rawValue) else {
                 return nil
             }
             return signupDate + " 가입"
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "signupDate")
+            UserDefaults.standard.set(newValue, forKey: key.signupDate.rawValue)
         }
     }
     
     static var searchList: [String: Date] {
         get {
-            guard let searchList = UserDefaults.standard.value(forKey: "searchList") as? [String: Date] else { return [:] }
+            guard let searchList = UserDefaults.standard.value(forKey: key.searchList.rawValue) as? [String: Date] else { return [:] }
             return searchList
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "searchList")
+            UserDefaults.standard.set(newValue, forKey: key.searchList.rawValue)
         }
     }
     
     static var wishList: [String: Bool] {
         get {
-            guard let wishList = UserDefaults.standard.dictionary(forKey: "wishList") as? [String: Bool] else { return [:] }
+            guard let wishList = UserDefaults.standard.dictionary(forKey: key.wishList.rawValue) as? [String: Bool] else { return [:] }
             return wishList
         }
         set {
-            UserDefaults.standard.set(newValue, forKey: "wishList")
+            UserDefaults.standard.set(newValue, forKey: key.wishList.rawValue)
         }
     }
     
@@ -87,5 +88,13 @@ struct User {
         for key in UserDefaults.standard.dictionaryRepresentation().keys {
             UserDefaults.standard.removeObject(forKey: key)
         }
+    }
+    
+    enum UserDataKey: String {
+        case nickname
+        case profileImageAssetName
+        case signupDate
+        case searchList
+        case wishList
     }
 }
